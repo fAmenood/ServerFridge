067698d00d805996e3e385657f5b7b2ef99a0ffb
Ivan Moroz <yaroslavyanushkevich2000@gmail.com>
2025-06-17 18:19:37 +0300

    Remove WeatherForecast controller and other files whose was connected with him, add Models and first table Fridge and AppDbContext for database

---
+++ ServerFridge/DataContext/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ServerFridge.DataContext
+{
+    public class AppDbContext:DbContext
+    { 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        
+    }
+}
